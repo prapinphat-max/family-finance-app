47435713c99046553f04591215bf5b51c47e2554
prapinphat-max <prapinphat@gmail.com>
2026-05-18 06:46:23 +0700

    add PWA icon

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Family Calendar',
+        short_name: 'FamilyCal',
+        description: 'Family schedule app',
+        theme_color: '#2D6E5C',
+        background_color: '#F5F7F3',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
